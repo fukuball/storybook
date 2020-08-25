86e4a946cfbcf4d082409cf06cd9122e07703204
Fukuball Lin <fukuball@gmail.com>
2020-08-25 11:19:30 +0800

    Build dist

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import Button from './stories/Button';
 import Header from "./stories/Header";
 import Page from "./stories/Page";
 
-export {
-  Button,
-  Header,
-  Page
+const Components = [Button, Header, Page];
+const install = function (Vue) {
+ Components.forEach(component => {
+   Vue.component(component.name, component)
+ })
+}
+
+export default {
+ install
 }
